255d05ac92906276d6c819300ed5681866188189
ktchethan <kt.chethan@gmail.com>
2021-09-10 15:48:42 +0530

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,30 @@
+const params = {};
+
+function addParams(p) {
+  for (const [key, value] of Object.entries(p)) {
+    if (!params[key]) params[key] = [];
+    params[key].push(value);
+  }
+}
+
+module.exports = {addParams, params};
+const params = {};
+
+function addParams(p) {
+  for (const [key, value] of Object.entries(p)) {
+    if (!params[key]) params[key] = [];
+    params[key].push(value);
+  }
+}
+
+module.exports = {addParams, params};
+const params = {};
+
+function addParams(p) {
+  for (const [key, value] of Object.entries(p)) {
+    if (!params[key]) params[key] = [];
+    params[key].push(value);
+  }
+}
+
+module.exports = {addParams, params};
